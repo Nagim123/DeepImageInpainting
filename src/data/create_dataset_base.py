1369c9b0e5704f80f20a323d370ac7dd839fba4b
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-09-29 17:16:32 +0300

    Create scripts

---
+++ src/data/create_dataset_base.py
@@ -0,0 +1,79 @@
+import torch
+from torch.utils.data import DataLoader, TensorDataset, random_split
+
+class MaskImageDataset:
+    """
+    Basic class to download clean images and generate masks.
+    """
+
+    def __init__(self, from_file=None):
+        """
+        Load dataset from file if specified.
+        Otherwise automaticly download and create from scratch.
+        """
+
+        if not from_file is None:
+            self.load_dataset(from_file)
+            return
+        # Load some images
+        self.images = self.load_images()[:10]
+        # Add masks to images
+        self.masked_images = torch.cat(
+            [self.mask_image(image).unsqueeze(0) for image in self.images]
+            , dim=0)[:10]
+
+    def load_images(self) -> torch.Tensor:
+        """
+        Load image from the internet.
+        """
+        return torch.tensor([])
+
+    def mask_image(self, image: torch.Tensor) -> torch.Tensor:
+        """
+        Create a mask for image.
+        """
+        return image
+
+    def get_image(self, index):
+        """
+        Give image by index.
+        """
+        return self.images[index]
+
+    def get_masked_image(self, index):
+        """
+        Give image with mask by index.
+        """
+        return self.masked_images[index]
+
+    def save_dataset(self, filename="dataset.pt"):
+        """
+        Save any dataset to a file.
+        """
+        torch.save({
+            "images": self.images,
+            "masked_images": self.masked_images
+        }, filename)
+
+    def load_dataset(self, filename="dataset.pt"):
+        """
+        Load a dataset from a file.
+        """
+        dataset = torch.load(filename)
+        self.images = dataset["images"]
+        self.masked_images = dataset["masked_images"]
+
+    def pack_to_dataloaders(self, batch_size=32, train_fraction=0.7) -> tuple[DataLoader, DataLoader]:
+        """
+        Pack image data into dataloaders for model training.
+        """
+        images_dataset = TensorDataset(self.masked_images, self.images)
+        images_train, images_val = random_split(images_dataset, [train_fraction, 1-train_fraction])
+        return (DataLoader(images_train, batch_size=batch_size, shuffle=False),
+                DataLoader(images_val, batch_size=batch_size, shuffle=True))
+
+    def __len__(self):
+        return self.images.shape[0]
+
+if __name__ == "__main__":
+    raise Exception("Do not call this script! It's base class!")
---
+++ src/models/model.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+
+class SimpleAE(nn.Module):
+    def __init__(self):
+        super(SimpleAE, self).__init__()
+        self.conv_layer = nn.Sequential(
+            nn.Conv2d(3, 32, (3,3), (1,1), 1),
+            nn.ReLU(),
+            nn.MaxPool2d((2,2)),
+            nn.Conv2d(3, 32, (3,3), (1,1), 1),
+            nn.ReLU(),
+            nn.MaxPool2d((2,2)),
+            nn.Conv2d(64, 128, (3,3), (1,1), "same"),
+            nn.ReLU(),
+            nn.MaxPool2d((2,2)),
+            nn.Conv2d(128, 256, (3,3), (1,1), "same"),
+            nn.ReLU(),
+            nn.MaxPool2d((2,2)),
+        )
+        self.deconv_layer == nn.Sequential(
+            nn.ConvTranspose2d(256, 128, (2,2), (2,2), "same"),
+            nn.ReLU(),
+            nn.MaxPool2d((2,2)),
+            nn.ConvTranspose2d(128, 64, (2,2), (2,2), "same"),
+            nn.ReLU(),
+            nn.MaxPool2d((2,2)),
+            nn.ConvTranspose2d(64, 32, (2,2), (2,2), "same"),
+            nn.ReLU(),
+            nn.MaxPool2d((2,2)),
+        )
+
+    def forward(self, x):
+        pass
