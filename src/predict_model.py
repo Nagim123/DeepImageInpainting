11ba27f22f6cc691fbf8af2676760c3b10ad9713
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-01 23:36:16 +0300

    Add manual file loading support

--- src/predict_model.py
+++ src/predict_model.py
@@ -10,18 +10,28 @@ script_path = pathlib.Path(__file__).parent.resolve()
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("model_name", type=str)
-    parser.add_argument("dataset", type=str)
-    parser.add_argument("image_index", type=int)
+    parser.add_argument("--image_path", type=str)
+    parser.add_argument("--dataset", type=str)
+    parser.add_argument("--image_index", type=int)
     args = parser.parse_args()
 
-    dataset = MaskImageDataset(from_file=args.dataset)
     path_to_model = os.path.join(script_path, f"models/checkpoints/{args.model_name}.pt")
     path_to_weights = os.path.join(script_path, f"models/checkpoints/{args.model_name}.pth")
+    
+    if args.image_path is None and args.dataset is None:
+        raise Exception("You must specify --image_path <path/to/image.png> or --dataset <name> --image_index <index>")
+
+    if args.image_path is None:
+        dataset = MaskImageDataset(from_file=args.dataset)
+        input = dataset.get_masked_image(args.image_index).unsqueeze(0)
+    else:
+        input = torch.tensor((np.asarray(Image.open(args.image_path).convert('RGB'))/255).astype(np.float32))
+        input = input.permute((2,0,1)).unsqueeze(0)
     model = torch.jit.load(path_to_model)
     model.eval()
     with torch.no_grad():
         model.load_state_dict(torch.load(path_to_weights))
-        input = dataset.get_image_val(args.image_index).unsqueeze(0)
+        
         output = model(input)
         matrix = output[0].permute((1, 2, 0)).numpy()
         image = Image.fromarray((matrix*255).astype(np.uint8))
