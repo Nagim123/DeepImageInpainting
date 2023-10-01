535c068f44f0483581cf95352a4e572789cb82c1
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-01 21:39:57 +0300

    Add prediction script

--- src/predict_model.py
+++ src/predict_model.py
@@ -1,6 +1,8 @@
+import numpy as np
+from PIL import Image
 import argparse
 import torch
-from models.model import CurrentModel
+from data.create_dataset_base import MaskImageDataset
 import pathlib
 import os
 script_path = pathlib.Path(__file__).parent.resolve()
@@ -12,6 +14,15 @@ if __name__ == "__main__":
     parser.add_argument("image_index", type=int)
     args = parser.parse_args()
 
-    current_model = CurrentModel()
-    model_scripted = torch.jit.script(current_model)
-    model_scripted.save(os.path.join(script_path, f"models/checkpoints/{args.model_name}.pt"))
+    dataset = MaskImageDataset(from_file=args.dataset)
+    path_to_model = os.path.join(script_path, f"models/checkpoints/{args.model_name}.pt")
+    path_to_weights = os.path.join(script_path, f"models/checkpoints/{args.model_name}.pth")
+    model = torch.jit.load(path_to_model)
+    model.eval()
+    with torch.no_grad():
+        model.load_state_dict(torch.load(path_to_weights))
+        input = dataset.get_masked_image(args.image_index).unsqueeze(0)
+        output = model(input)
+        matrix = output[0].permute((1, 2, 0)).numpy()
+        image = Image.fromarray((matrix*255).astype(np.uint8))
+        image.save(os.path.join(script_path, "data/temp/prediction.png"))
