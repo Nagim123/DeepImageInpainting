301300dcdd2d50ecc8749c55ec8b846307ca65a4
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-02 21:48:24 +0300

    Fix optimizer for GAN

--- src/train_model.py
+++ src/train_model.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     # Loading Discriminator model (if required)
     if args.GAN_model:
         discriminator, discriminator_save_path = load_model(args.model_name, args.weights, CurrentDiscriminatorModel)
-        disc_optimizer = torch.optim.Adam(model.parameters())
+        disc_optimizer = torch.optim.Adam(discriminator.parameters())
 
     train_loader, val_loader = MaskImageDataset(from_file=args.dataset).pack_to_dataloaders(batch_size=32)
     loss_fn = loss_functions[args.loss]()
