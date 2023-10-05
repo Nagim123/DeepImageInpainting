3669ac6ba5d216e62f1d4665bb8d5105dccb9b21
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-05 11:55:17 +0300

    Upgrade discrimantor

--- src/models/discriminator_model.py
+++ src/models/discriminator_model.py
@@ -4,15 +4,19 @@ class CurrentDiscriminatorModel(nn.Module):
     def __init__(self):
         super(CurrentDiscriminatorModel, self).__init__()
         self.conv_layer = nn.Sequential(
-            nn.Conv2d(3, 32, (3,3), (2,2), 1),
+            nn.Conv2d(3, 32, (4,4), (2,2), 1),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(0.2),
-            nn.Conv2d(32, 64, (3,3), (2,2), 1),
+            nn.Conv2d(32, 64, (4,4), (2,2), 1),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2),
-            nn.Conv2d(64, 128, (3,3), (2,2), 1),
+            nn.Conv2d(64, 128, (4,4), (2,2), 1),
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
-            nn.Conv2d(128, 256, (3,3), (2,2), 1),
+            nn.Conv2d(128, 256, (4,4), (2,2), 1),
+            nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2),
-            nn.Conv2d(256, 1, (3,3), (2,2), 1),
+            nn.Conv2d(256, 1, (4,4), (2,2), 1),
             nn.Sigmoid()
         )
 
