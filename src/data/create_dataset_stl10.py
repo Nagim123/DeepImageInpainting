9a8a89ce716b3f06f71f451e51f7e3ba8b246c3c
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-23 19:16:07 +0300

    Add stl10 dataset to files

---
+++ src/data/create_dataset_stl10.py
@@ -0,0 +1,40 @@
+from create_dataset_base import MaskImageDataset
+import tensorflow_datasets as tfds
+import torch
+import numpy as np
+
+class STL10Masked(MaskImageDataset):
+    """
+    STL10 dataset with 96x96 RGB images.
+    """
+
+    def load_images(self) -> np.array:
+        # Load images using tensorflow
+        images = tfds.load('stl10',
+                        split='train',
+                        #split=['train', 'test'],
+                        data_dir="/content/dataset")
+        torch_data = torch.tensor([img['image'].numpy()/255 for img in images], dtype=torch.float32)
+        # torch_data = torch.tensor(images, dtype=torch.float32)/255
+        return torch_data.permute((0, 3, 1, 2))
+
+    def mask_image(self, image: torch.Tensor) -> np.array:
+        image = image.permute((1, 2, 0))
+        # Generate random top-left coordinates.
+        x1, y1 = np.random.randint(0, 26, size=2)
+        # Generate random bottom-right coordinates
+        min_w, min_h, max_w, max_h = 10, 10, 27, 27
+        x2 = np.random.randint(min(x1+min_w,96), min(x1+max_w,96))
+        y2 = np.random.randint(min(y1+min_h,96), min(y1+max_h,96))
+        # Copy original image
+        masked_image = image.clone().detach()
+        # Fill black rectange on generated coordinates
+        mask = torch.tensor(np.zeros((y2-y1)*(x2-x1)*3).reshape(y2-y1,x2-x1,3))
+        masked_image[y1:y2,x1:x2] = mask
+        # Return result
+        return masked_image.permute((2, 0, 1))
+
+if __name__ == "__main__":
+    stl10_dataset = STL10Masked()
+    stl10_dataset.save_dataset(filename="cifar10.pt")
+    print("CIFAR10 dataset is created and saved in datasets\cifar10.pt")
---
+++ src/utils/trainer.py
@@ -0,0 +1,81 @@
+
+import torch
+
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from torch.nn import Module
+
+class Trainer:
+    def __init__(self, model: Module, train_loader: DataLoader, val_loader: DataLoader, loss_fn, ) -> None:
+        pass
+
+def train_one_epoch(model, train_loader, loss_fn, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    total = 0
+    progress = tqdm(enumerate(train_loader), total=len(train_loader))
+    for i, batch in progress:
+        input, target = batch
+        input, target = input.to(device), target.to(device)
+        total += 1
+        optimizer.zero_grad()
+        reconstruction = model(input).to(device)
+        loss = loss_fn(reconstruction, target)
+        loss.backward()
+        running_loss += loss.item()
+        optimizer.step()
+        progress.set_postfix({"loss": loss.item()})
+    return running_loss / total
+
+def train_one_epoch_with_discriminator(generator, discriminator, train_loader, loss_fn, opt_gen, opt_disc, device):
+    generator.train()
+    discriminator.train()
+    running_loss = 0.0
+    total = 0
+    progress = tqdm(enumerate(train_loader), total=len(train_loader))
+    for i, batch in progress:
+        input, target = batch
+        input, target = input.to(device), target.to(device)
+        # Generate fake image
+        fake = generator(input).to(device)
+        total += 1
+
+        # Training Discriminator
+        opt_disc.zero_grad()
+        disc_real = discriminator(target).to(device).reshape(-1)
+        loss_disc_real = loss_fn(disc_real, torch.ones_like(disc_real))
+        disc_fake = discriminator(fake.detach()).to(device).reshape(-1)
+        loss_disc_fake = loss_fn(disc_fake, torch.zeros_like(disc_fake))
+        loss_disc = (loss_disc_real + loss_disc_fake) / 2
+
+        
+        loss_disc.backward()
+        opt_disc.step()
+
+        # Training Generator
+        opt_gen.zero_grad()
+        output = discriminator(fake).to(device).reshape(-1)
+        loss_gen = (loss_fn(output, torch.ones_like(output)) + loss_fn(fake, target))
+        loss_gen.backward()
+        opt_gen.step()
+        running_loss += loss_gen.item()
+        progress.set_postfix({"gen_loss": loss_gen.item(), "dicr_loss": loss_disc.item()})
+
+    return running_loss / total
+
+def val_one_epoch(model, val_loader, loss_fn, device):
+    with torch.no_grad():
+        model.eval()
+        running_loss = 0.0
+        total = 0
+        progress = tqdm(enumerate(val_loader), total=len(val_loader))
+        for i, batch in progress:
+            input, target = batch
+            input, target = input.to(device), target.to(device)
+
+            total += 1
+            reconstruction = model(input).to(device)
+            loss = loss_fn(reconstruction, target)
+            running_loss += loss.item()
+            progress.set_postfix({"e_loss": loss.item()})
+        return running_loss / total
