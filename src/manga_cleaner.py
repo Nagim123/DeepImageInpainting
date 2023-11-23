e2d736c76b0747781591dd6037dfdcf9a286278f
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-11-24 00:52:11 +0300

    Fix bugs 1

--- src/manga_cleaner.py
+++ src/manga_cleaner.py
@@ -5,10 +5,11 @@ import argparse
 print(script_path)
 
 TEMP_FOLDER_PATH = os.path.join(script_path, "../temp/")
-UNET_MODEL_PATH = os.path.join(script_path, "models")
+UNET_MODEL_PATH = os.path.join(script_path, "models/")
 MANGA_SEG_MODEL_PATH = os.path.join(script_path, "models/comictextdetector.pt")
 SEGMENT_SCRIPT_PATH = os.path.join(script_path, "comic-text-detector/run_model.py")
 PREDICT_SCRIPT_PATH = os.path.join(script_path, "predict_model.py")
+OUTPUTS_PATH = os.path.join(script_path, "../outputs/")
 
 if __name__ == "__main__":
     # Parse command line arguments
@@ -17,8 +18,33 @@ if __name__ == "__main__":
     parser.add_argument("--manga_dir", type=str)
     args = parser.parse_args()
 
-    # Check if model's path specified by user
+    # Check if directory with manga specified
     if args.manga_dir is None:
         raise Exception("You should specify directory with all manga pages in --manga_dir")
 
+    # Get all filenames from manga directory
+    image_names = []
+    for (dirpath, dirnames, filenames) in os.walk(args.manga_dir):
+        image_names.extend(filenames)
+        break
+
+    # Run mask extractor
     os.system(f"python {SEGMENT_SCRIPT_PATH} --image_dir {args.manga_dir} --output_dir {TEMP_FOLDER_PATH} --model_path {MANGA_SEG_MODEL_PATH}")
+
+    for image_name in image_names:
+        # Prepare paths
+        path_to_img = os.path.join(TEMP_FOLDER_PATH, image_name)
+        path_to_mask = os.path.join(TEMP_FOLDER_PATH, f"mask-{image_name}")
+        output_path = os.path.join(OUTPUTS_PATH, image_name)
+
+        # Run inpainting model        
+        os.system(f"python {PREDICT_SCRIPT_PATH} --image_path {path_to_img} --image_mask_path {path_to_mask} --model_path {UNET_MODEL_PATH} --output_file {output_path}")
+        
+        # Remove temporary files
+        os.remove(path_to_img)
+        os.remove(path_to_mask)
+        clean_name = image_name.split(".")[0]
+        os.remove(os.path.join(TEMP_FOLDER_PATH, f"{clean_name}.txt"))
+        os.remove(os.path.join(TEMP_FOLDER_PATH, f"line-{clean_name}.txt"))
+
+
--- src/predict_model.py
+++ src/predict_model.py
@@ -1,9 +1,25 @@
 import numpy as np
 import argparse
 import torch
-from data.create_dataset_base import MaskImageDataset
 from utils.model_loader import load_model
 from PIL import Image
+
+def apply_mask_to_img(orig_image: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+    """
+    Make pixels on image black if they are masked.
+
+    Parameters:
+        orig_image (Tensor): Original image to apply mask on.
+        mask (Tensor): Image mask.
+
+    Returns:
+        Tensor: Masked image.
+    """
+    # Get locations of mask pixels
+    masked_images = orig_image.detach().clone()
+    locations = (mask == 1).nonzero()
+    masked_images[:, locations[:, 0], locations[:, 1]] = 0
+    return masked_images
 
 if __name__ == "__main__":
     
@@ -13,6 +29,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_path", type=str)
     parser.add_argument("--image_path", type=str)
     parser.add_argument("--image_mask_path", type=str)
+    parser.add_argument("--output_file", type=str)
     args = parser.parse_args()
 
     # Check if model's path specified by user
@@ -29,15 +46,17 @@ if __name__ == "__main__":
 
     # Read image
     image = torch.tensor((np.asarray(Image.open(args.image_path).convert('RGB'))/255).astype(np.float32))
-    image = image.permute((2,0,1)).unsqueeze(0)
+    image = image.permute((2,0,1))
 
     # Read mask
     mask = torch.tensor((np.asarray(Image.open(args.image_mask_path).convert('L'))/255).astype(np.float32))
+
+    # Apply mask on image
+    masked_img = apply_mask_to_img(image, mask).unsqueeze(0)
     mask = mask.unsqueeze(0).unsqueeze(0)
 
     # Concatinate image with mask
-    input = torch.cat([image, mask], dim=1)
-    print(input.shape)
+    input = torch.cat([masked_img, mask], dim=1)
 
     # Do inference
     model = load_model(f"{args.model_path}/unet_model.pt", f"{args.model_path}/unet_weights.pt")
@@ -46,4 +65,7 @@ if __name__ == "__main__":
         output = model(input)
         matrix = output[0].permute((1, 2, 0)).numpy()
         image = Image.fromarray((matrix*255).astype(np.uint8))
-        image.save("prediction.png")
+        if args.output_file is None:
+            image.save("prediction.png")
+        else:
+            image.save(args.output_file)
