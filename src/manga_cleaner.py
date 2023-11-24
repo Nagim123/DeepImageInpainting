14f1cd67caeed84336691bfa8bb68824c990d22c
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-11-24 19:57:43 +0300

    Update manga_cleaner.py

--- src/manga_cleaner.py
+++ src/manga_cleaner.py
@@ -2,6 +2,8 @@ import pathlib
 import os
 script_path = pathlib.Path(__file__).parent.resolve()
 import argparse
+import numpy as np
+import cv2
 print(script_path)
 
 TEMP_FOLDER_PATH = os.path.join(script_path, "../temp/")
@@ -11,10 +13,39 @@ SEGMENT_SCRIPT_PATH = os.path.join(script_path, "comic-text-detector/run_model.p
 PREDICT_SCRIPT_PATH = os.path.join(script_path, "predict_model.py")
 OUTPUTS_PATH = os.path.join(script_path, "../outputs/")
 
+
+def dilate_rgb_mask(mask: np.array, kernel_size: int = 7) -> np.array:
+    """
+    Dilate mask lines width in image for better performance.
+
+    Parameters:
+        mask (np.array): Link for download
+        kernel_size (int): The value of dilate. Default is 7.
+    
+    Return:
+        (np.array): Image with increased lines mask width.
+    """
+    # Separate the channels
+    b, g, r = cv2.split(mask)
+
+    # Create a kernel for dilation
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+
+    # Dilate each channel independently
+    dilated_b = cv2.dilate(b, kernel, iterations=1)
+    dilated_g = cv2.dilate(g, kernel, iterations=1)
+    dilated_r = cv2.dilate(r, kernel, iterations=1)
+
+    # Merge the dilated channels back into an RGB image
+    dilated_mask = cv2.merge([dilated_b, dilated_g, dilated_r])
+
+    return dilated_mask[:,:,0]
+
 if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser(description="Script that clean manga from symbols")
-
+    
+    parser.add_argument("--line_width", type=int, default=7)
     parser.add_argument("--manga_dir", type=str)
     args = parser.parse_args()
 
@@ -37,6 +68,10 @@ if __name__ == "__main__":
         path_to_mask = os.path.join(TEMP_FOLDER_PATH, f"mask-{image_name}")
         output_path = os.path.join(OUTPUTS_PATH, image_name)
 
+        masked_img = cv2.imread(path_to_mask)
+        masked_img = dilate_rgb_mask(masked_img, kernel_size=args.line_width)
+        cv2.imwrite(path_to_mask, masked_img)
+
         # Run inpainting model        
         os.system(f"python {PREDICT_SCRIPT_PATH} --image_path {path_to_img} --image_mask_path {path_to_mask} --model_path {UNET_MODEL_PATH} --output_file {output_path}")
         
@@ -46,5 +81,3 @@ if __name__ == "__main__":
         clean_name = image_name.split(".")[0]
         os.remove(os.path.join(TEMP_FOLDER_PATH, f"{clean_name}.txt"))
         os.remove(os.path.join(TEMP_FOLDER_PATH, f"line-{clean_name}.txt"))
-
-
